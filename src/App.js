f636f5ec7d308249e71a1f701efbe801c8dd1e70
Iryna S <irynaradkovska@gmail.com>
2022-04-28 08:55:50 +0300

    додає апдейти

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import ColorPicker from "./components/ColorPicker";
+import Alert from "./components/Alert";
 
-const colorPickerOptions = [
-  { label: "red", color: "#F44336" },
-  { label: "green", color: "#4CAF50" },
-  { label: "blue", color: "#2196F3" },
-  { label: "grey", color: "#607D8B" },
-  { label: "pink", color: "#E91E63" },
-  { label: "indigo", color: "#3F51B5" },
-];
+// const colorPickerOptions = [
+//   { label: "red", color: "#F44336" },
+//   { label: "green", color: "#4CAF50" },
+//   { label: "blue", color: "#2196F3" },
+//   { label: "grey", color: "#607D8B" },
+//   { label: "pink", color: "#E91E63" },
+//   { label: "indigo", color: "#3F51B5" },
+// ];
 
 function App() {
   return (
     <div className="App">
-      <ColorPicker options={colorPickerOptions} />
+      <Alert text="Ей" />
+      <Alert text="Ей" type="success" />
+      <Alert text="Ей" type="warning" />
+      <Alert text="Ей" type="error" />
+      {/* <ColorPicker options={colorPickerOptions} /> */}
     </div>
   );
 }
